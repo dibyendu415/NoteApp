2245c0b5b5b24369feeaccd8424d0a9b6d98fa81
dibyendu415 <dibyendumaji415@gmail.com>
2020-07-01 02:29:37 +0530

    Updates

---
+++ precache-manifest.492884677aea8f2c12303a334b5256ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c93007a60540746c88c86af737e481dd",
+    "url": "/NoteApp/index.html"
+  },
+  {
+    "revision": "17c1550bdb375eeb19d4",
+    "url": "/NoteApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f89ce3bec99523948ee1",
+    "url": "/NoteApp/static/css/main.cc266d39.chunk.css"
+  },
+  {
+    "revision": "17c1550bdb375eeb19d4",
+    "url": "/NoteApp/static/js/2.8a7106ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NoteApp/static/js/2.8a7106ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f89ce3bec99523948ee1",
+    "url": "/NoteApp/static/js/main.698c967e.chunk.js"
+  },
+  {
+    "revision": "36aa2a843e96e82a1c5c",
+    "url": "/NoteApp/static/js/runtime-main.eb19e934.js"
+  }
+]);
